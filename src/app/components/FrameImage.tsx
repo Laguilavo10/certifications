f186551e65cf1297df41446c13976f4120ae71e0
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-05-10 19:43:37 -0500

    feat: change appearance frameImage

--- src/app/components/FrameImage.tsx
+++ src/app/components/FrameImage.tsx
@@ -2,18 +2,18 @@ import { PropsWithChildren } from 'react'
 
 export const FrameImage = (props: PropsWithChildren) => {
   return (
-    <div className='flex relative box-border h-full w-[370px] cursor-pointer rounded-md border-[7px] border-black bg-[#B27B39]  '>
-      <div className='m-auto my-6 h-5/6 w-5/6 rounded-md border-[7px] border-black hover:scale-110 transition-all duration-300 ease-in-out z-10'>
+    <div className='relative box-border flex h-full w-[370px] rounded-md border-[4px] border-black bg-[#4e320f]  '>
+      <div className=' z-10 m-auto my-6 h-5/6 w-5/6 justify-center rounded-md border-[4px] border-black transition-all duration-300 ease-in-out hover:scale-110'>
         {props.children}
       </div>
+      <span className='absolute -top-[11px] left-[13px] flex h-[46px] w-[4px] rotate-[130deg] rounded-md bg-black ' />
+      <span className='absolute -top-[11px] right-[13px] flex h-12 w-[4px] rotate-[230deg] rounded-md bg-black ' />
+      <span className='absolute -bottom-[11px] right-[13px] flex h-12 w-[4px] rotate-[130deg] rounded-md bg-black ' />
+      <span className='absolute -bottom-[11px] left-[13px] flex h-12 w-[4px] rotate-[230deg] rounded-md bg-black ' />
 
-      <span className='absolute -top-[11px] left-3 flex h-12 w-[7px] rotate-[130deg] rounded-md bg-black' />
-      <span className='absolute -top-[11px] right-3 flex h-12 w-[7px] rotate-[230deg] rounded-md bg-black' />
-      <span className='absolute -bottom-[11px] right-3 flex h-12 w-[7px] rotate-[130deg] rounded-md bg-black' />
-      <span className='absolute -bottom-[11px] left-3 flex h-12 w-[7px] rotate-[230deg] rounded-md bg-black' />
-
-      <span className='absolute -top-[56px] left-[154px] flex h-16 w-[7px] rotate-[45deg] rounded-md bg-black' />
-      <span className='absolute -top-[56px] right-[154px] flex h-16 w-[7px] rotate-[135deg] rounded-md bg-black' />
+      <span className='absolute -top-[56px] left-[160px] flex h-16 w-[4px] rotate-[45deg] rounded-md bg-black' />
+      <span className='absolute -top-[56px] right-[154px] flex h-16 w-[4px] rotate-[135deg] rounded-md bg-black' />
+      <span className='absolute -top-[56px] right-[172px] flex h-3 w-3 rounded-full  bg-yellow-500 border border-black' />
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,28 @@
 import Image from 'next/image'
 import { FrameImage } from './components/FrameImage'
+import { ImageZoom } from './components/ImageZoom'
+import { getImages } from './utils/getImages'
 
 export default async function Home() {
-  const getImages = async () => {
-    const URL_API = `https://api.cloudinary.com/v1_1/dyqdtw07b/resources/search/`
-    const API_KEY = '381343614963598'
-    const API_SECRET = 'HEu2KQMWx7sNfdcU3PxwyVgF0uo'
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${Buffer.from(
-          `${API_KEY}:${API_SECRET}`
-        ).toString('base64')})}`
-      },
-      body: JSON.stringify({ expression: 'folder=Certifications' })
-    }
-    const data = await fetch(URL_API, options)
-    const images = await data.json()
-    return images
-  }
   const { resources } = await getImages()
   return (
     <>
-      <main className=' h-full w-full bg-image'>
+      <main className=' bg-image min-w-screen h-full min-h-screen w-full'>
         <div className=' m-auto grid w-4/5 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] items-center justify-center gap-4 gap-x-40 gap-y-20 md:w-full md:p-14 '>
           {resources.map((image: any) => (
             <article
               className='rounded-mdshadow-md relative flex h-[280px] w-full flex-col items-center justify-center md:h-[300px]'
               key={image.public_id}>
               <FrameImage>
-                <Image
-                  src={image.url}
-                  width={500}
-                  height={500}
-                  alt={`diploma ${image.filename}`}
-                  className='aspect-[4/3] h-full w-full'
-                />
+                <ImageZoom>
+                  <Image
+                    src={image.url}
+                    width={500}
+                    height={500}
+                    alt={`diploma ${image.filename}`}
+                    className='aspect-[4/3] h-full w-full'
+                  />
+                </ImageZoom>
               </FrameImage>
             </article>
           ))}
