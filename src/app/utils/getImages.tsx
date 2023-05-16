700f92e21a512847cc71373d18b8aa1ca4ddbfc0
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-05-16 16:55:44 -0500

    fix: remove NEXT_PUBLIC from env variables

--- src/app/utils/getImages.tsx
+++ src/app/utils/getImages.tsx
@@ -5,12 +5,12 @@ export const getImages = async () => {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Basic ${Buffer.from(
-        `${process.env.NEXT_PUBLIC_API_KEY}:${process.env.NEXT_PUBLIC_API_SECRET}`
+        `${process.env.API_KEY}:${process.env.API_SECRET}`
       ).toString('base64')})}`
     },
     body: JSON.stringify({ expression: 'folder=Certifications/*' })
   }
-  const data = await fetch(process.env.NEXT_PUBLIC_URL_API as string, options)
+  const data = await fetch(process.env.URL_API as string, options)
   const images = await data.json()
   return images
 }
