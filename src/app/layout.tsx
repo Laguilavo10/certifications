5cf1d90c282894a1e3639cfb7615729c43f90a2c
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-05-17 17:13:06 -0500

    feat : add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+import type { Metadata } from 'next'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({ weight: '500', subsets: ['latin'] })
 
-export const metadata = {
+export const metadata : Metadata = {
   title: 'Certificados',
-  description: 'Certifcados de Andres Laguilavo realcionados a el area T.I'
+  description: 'Certifcados de Andres Laguilavo realcionados a el area T.I',
+  icons: {
+    icon: 'diploma.svg',
+  },
 }
 
 export default function RootLayout({
@@ -15,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <link rel='shortcut icon' href='/public/diploma.svg' type='image/x-icon'/>
       <body className={poppins.className}>{children}</body>
     </html>
   )
