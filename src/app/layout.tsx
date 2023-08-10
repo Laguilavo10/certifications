3b23055eb3ca88bb3c1bcf32665c934b0d955dc8
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-08-10 14:17:40 -0500

    core: add nextUI library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { UIProvider } from './components/UIProvider'
+
+import Header from './components/Header'
 
 const poppins = Poppins({ weight: '500', subsets: ['latin'] })
 
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: 'Certificados',
   description: 'Certifcados de Andres Laguilavo realcionados a el area T.I',
   icons: {
-    icon: 'diploma.svg',
-  },
+    icon: 'diploma.svg'
+  }
 }
 
 export default function RootLayout({
@@ -18,9 +22,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <link rel='shortcut icon' href='/public/diploma.svg' type='image/x-icon'/>
-      <body className={poppins.className + 'w-full h-full bg-image'}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <link
+          rel='shortcut icon'
+          href='/public/diploma.svg'
+          type='image/x-icon'
+        />
+        <body className={poppins.className + 'h-full w-full dark'}>
+          <UIProvider>
+            <Header />
+            {children}
+          </UIProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
