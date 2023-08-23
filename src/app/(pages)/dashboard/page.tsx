fe61087747bbe36e76165176bffe4258d3eac17c
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-08-23 13:46:14 -0500

    feat: change dashboard buttons

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,20 +1,21 @@
 import ButtonsDashboard from '@/app/components/ButtonsDashboard'
 import CardCertification from '@/app/components/CardCertification'
 import { getCertifications } from '@/app/services/getCertifications'
+import { getUsername } from '@/app/services/getUsername'
 import type { Certification } from '@/app/types/types'
+import { currentUser } from '@clerk/nextjs'
 
 export default async function Dashboard() {
-  const resources = await getCertifications('lagui2003@gmail.com')
+  const user = await currentUser()
+  const email = user?.emailAddresses[0]?.emailAddress
+  if (!email) return
+  const { username } = await getUsername(email)
+  const resources = await getCertifications(username)
   return (
     <main className='m-auto h-full min-h-screen max-w-8xl pt-14'>
-      {/* <SearchBar
-        value={searchValue}
-        setValue={setSearchValue}
-        className='m-auto w-2/6'
-      /> */}
       <section className='relative flex flex-col gap-10 px-5 py-10'>
-      <ButtonsDashboard/>
-        {resources.map((certification: Certification) => (
+        <ButtonsDashboard username={resources.username} />
+        {resources?.certifications?.map((certification: Certification) => (
           <CardCertification
             key={certification._id}
             certification={certification}
--- src/app/components/ButtonsDashboard.tsx
+++ src/app/components/ButtonsDashboard.tsx
@@ -2,21 +2,18 @@
 import { Button } from '@nextui-org/react'
 import { ArrowTopRightOnSquareIcon as RedirectIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-// import CreateEntity from './CreateEntity'
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+
 interface Props {
   username: `${string}-${string}-${string}`
 }
-export default function ButtonsDashboard({ username }: Props) {
-  console.log(username)
 
+export default function ButtonsDashboard({ username }: Props) {
   return (
     <div className='flex w-full justify-end gap-5'>
-      <Button>
-        <Link href={`/user/${username}`}>{`/user/${username}`}</Link>
+      <Button endContent={<RedirectIcon className='h-5' />}>
+        <Link href={`/user/${username}`} target='_blank'>{`/user/${username}`}</Link>
       </Button>
       <Button
-        endContent={<RedirectIcon className='h-5' />}
         color='primary'
         className='text-black'
       >
