c557b9e6faa155a8a073de94a26c39a5a46a413f
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-10-01 19:26:36 -0500

    fix: problem with lastname in webhook

--- src/app/services/createUsername.ts
+++ src/app/services/createUsername.ts
@@ -2,8 +2,14 @@ import { v4 as uuidv4 } from 'uuid'
 
 export const createUsername = (firstName: string, lastName: string) => {
   const firstWordFirstName = firstName.split(' ')[0].toLowerCase() // Tomar la primera palabra del primer nombre
-  const firstWordLastName = lastName.split(' ')[0].toLowerCase() // Tomar la primera palabra del primer apellido
-  const uuid = uuidv4().split('-')[0]
-  const username = `${firstWordFirstName}-${firstWordLastName}-${uuid}`
-  return username
+  if (!lastName) {
+    const uuid = uuidv4().split('-')[0]
+    const username = `${firstWordFirstName}-${uuid}`
+    return username
+  } else {
+    const firstWordLastName = lastName.split(' ')[0].toLowerCase() // Tomar la primera palabra del primer apellido
+    const uuid = uuidv4().split('-')[0]
+    const username = `${firstWordFirstName}-${firstWordLastName}-${uuid}`
+    return username
+  }
 }
